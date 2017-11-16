11504e5c968efd6e5e93f399f1212c54ae7560c5
Comicer88 <dominik.bartosch@hotmail.de>
2017-11-16 14:57:44 +0100

    Login Functions and registry
    For you Günter pls make that shit

---
+++ pages/login.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=php-einfach', 'root', '');
+
+if(isset($_GET['login'])) {
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
+
+    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $result = $statement->execute(array('email' => $email));
+    $user = $statement->fetch();
+
+    //Überprüfung des Passworts
+    if ($user !== false && password_verify($passwort, $user['passwort'])) {
+        $_SESSION['userid'] = $user['id'];
+        die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
+    } else {
+        $errorMessage = "E-Mail oder Passwort war ungültig<br>";
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Registrierung</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<?php
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+
+<form action="?login=1" method="post">
+    E-Mail:<br>
+    <input type="email" size="40" maxlength="250" name="email"><br><br>
+
+    Dein Passwort:<br>
+    <input type="password" size="40"  maxlength="250" name="passwort"><br>
+
+    <input type="submit" value="Abschicken">
+</form>
+</body>
+</html>
---
+++ pages/registrieren.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Registrierung LogDrive</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+<?php
+$showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+
+if(isset($_GET['register'])) {
+    $error = false;
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
+    $passwort2 = $_POST['passwort2'];
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+        $error = true;
+    }
+    if(strlen($passwort) == 0) {
+        echo 'Bitte ein Passwort angeben<br>';
+        $error = true;
+    }
+    if($passwort != $passwort2) {
+        echo 'Die Passwörter müssen übereinstimmen<br>';
+        $error = true;
+    }
+
+    //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+    if(!$error) {
+        $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+        $result = $statement->execute(array('email' => $email));
+        $user = $statement->fetch();
+
+        if($user !== false) {
+            echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+            $error = true;
+        }
+    }
+
+    //Keine Fehler, wir können den Nutzer registrieren
+    if(!$error) {
+        $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+
+        $statement = $pdo->prepare("INSERT INTO users (email, passwort) VALUES (:email, :passwort)");
+        $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash));
+
+        if($result) {
+            echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+            $showFormular = false;
+        } else {
+            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+        }
+    }
+}
+
+if($showFormular) {
+    ?>
+
+    <form action="?register=1" method="post">
+        E-Mail:<br>
+        <input type="email" size="40" maxlength="250" name="email"><br><br>
+
+        Dein Passwort:<br>
+        <input type="password" size="40"  maxlength="250" name="passwort"><br>
+
+        Passwort wiederholen:<br>
+        <input type="password" size="40" maxlength="250" name="passwort2"><br><br>
+
+        <input type="submit" value="Abschicken">
+    </form>
+
+    <?php
+} //Ende von if($showFormular)
+?>
+
+</body>
+</html>
